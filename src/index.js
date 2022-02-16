0ba282b540c0c3733a90a266c1d189a1f3ac5b5b
Lucas Mendes <lucas.oliviero@sotran.com.br>
2022-02-16 08:32:56 -0300

    aula de middlewares

--- src/index.js
+++ src/index.js
@@ -3,22 +3,32 @@ const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const { process_params } = require("express/lib/router");
 const { response } = require("express");
+const req = require("express/lib/request");
 
 const app = express();
 app.use(express.json());
 
 const costomers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement - []
- */
+// Middleware
+function verifyIfExistsAccountCpf(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = costomers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({message: "Costomer not found!"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
-    const costomerAlreadyExists = costomers.some((costomer) => costomer.cpf === cpf);
+    const costomerAlreadyExists = costomers.some((customer) => customer.cpf === cpf);
     
     if(costomerAlreadyExists){
         return response.status(400).json({
@@ -36,14 +46,12 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
-
-    const costomer = costomers.find(costomer => costomer.cpf === cpf);
-
+app.get("/statement", verifyIfExistsAccountCpf, (request, response) => {
+    const { customer } = request;
+    
     return response.json({
-        statement: costomer.statement
-    })
+        statement: customer.statement
+    });
 });
 
 app.listen(3333);
